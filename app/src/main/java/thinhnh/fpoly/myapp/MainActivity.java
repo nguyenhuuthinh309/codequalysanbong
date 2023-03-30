c3d0dc8f6f9a909f2ec72b0cbc1fb8bfdaae8988
Laptop <phuoclhph21930@fpt.edu.vn>
2023-03-30 14:36:29 +0700

    303

--- app/src/main/java/thinhnh/fpoly/myapp/MainActivity.java
+++ app/src/main/java/thinhnh/fpoly/myapp/MainActivity.java
@@ -1,11 +1,12 @@
 package thinhnh.fpoly.myapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

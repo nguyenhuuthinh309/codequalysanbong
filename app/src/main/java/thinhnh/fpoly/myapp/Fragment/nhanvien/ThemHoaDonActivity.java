6d278d6dedd0dfbe330927d38a559ccd926fc032
Laptop <phuoclhph21930@fpt.edu.vn>
2023-04-01 14:34:12 +0700

    303/04

--- app/src/main/java/thinhnh/fpoly/myapp/Fragment/nhanvien/ThemHoaDonActivity.java
+++ app/src/main/java/thinhnh/fpoly/myapp/Fragment/nhanvien/ThemHoaDonActivity.java
@@ -32,7 +32,7 @@ import thinhnh.fpoly.myapp.csdl.DTO.TrangThaiHoaDon;
 import thinhnh.fpoly.myapp.csdl.data.DataBaSe;
 
 public class ThemHoaDonActivity extends AppCompatActivity {
-    //jgchgachasvcg
+    //jgchgachasvcg//uxryctugigyftrdxcv
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
     int myear,mmonth,mday;
 
